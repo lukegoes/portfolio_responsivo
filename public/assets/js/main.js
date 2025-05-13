bd6450cd234a989f422b6b6b5931fcc99a8f0eb8
Luke <luklg@outlook.com>
2025-05-12 23:28:41 -0300

    fix: ajustes nas cores

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -148,6 +148,6 @@ const sr = ScrollReveal({
 
 sr.reveal(`.home__perfil, .about__image, .about__shadow, .contact__mail`, {origin : "right"})
 sr.reveal(`.home__name`, `.home__info`,
-            `.about__container .section__title-1, .about__info,
+            ` .section__title-1, .about__info,
             .contact__social, .contact__data`,{origin : "left"})
-sr.reveal(`.services__card, .projects__card`,{interval: 100})            
+sr.reveal(`.services__card, .projects__card , .about__container`,{interval: 100})            
